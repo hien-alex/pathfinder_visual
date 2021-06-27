91ea51d958e1180876b1acf051aa8a7b5077ca55
alex-hien <hiennalex@gmail.com>
2021-06-27 11:16:01 -0700

    project setup

---
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class PathfindingVisualizer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>hi</div>;
+  }
+}
